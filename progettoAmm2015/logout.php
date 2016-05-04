ebf617f02a809fc9fa3c4b45a202dee51c6b4273
PoddaAndrea <andrea.podda.88@gmail.com>
2016-05-04 19:30:46 +0200

    commit

--- progettoAmm2015/logout.php
+++ progettoAmm2015/logout.php
@@ -5,5 +5,5 @@
 	session_start();
 	unset($_SESSION['sess_user']); //viene "inserita" la sessione che successivamente verrà "distrutta" 
 	session_destroy();// distrugge la sessione
-	header("location: progettoAmm2015/index.php" );//riporta alla pagina index
+	header("location: index.php");//riporta alla pagina index
 ?>
